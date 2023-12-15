2c7565d99656a31a6b0af2d4bf3d9294cdbe9c4b
mopebad <joshmopebad@gmail.com>
2023-12-15 02:19:43 -0500

    final

--- Sketch.java
+++ Sketch.java
@@ -1,131 +1,293 @@
+import java.util.ArrayList;
 import processing.core.PApplet;
+import processing.core.PImage;
+import processing.core.PVector;
+import processing.event.MouseEvent;
 
-/**
- * Processing Task 8
- * Author: Joshua Chan
- */
 public class Sketch extends PApplet {
 
+  // Images for background, flowers, and dog
+  PImage imgBackground;
+  PImage imgFlower;
+  PImage imgBackground2;
+  PImage imgDog;
+
+  // ArrayLists to store positions of flowers and raindrops
+  ArrayList<PVector> flowers = new ArrayList<PVector>();
+  ArrayList<Raindrop> raindrops = new ArrayList<Raindrop>();
+
+  // Flags for various actions
+  boolean isDragging = false;
+  boolean isFlowerClicked = false;
+
+  // Sun size and location variables
+  int intSunSizeX = 30;
+  int intSunSizeY = 30;
+  int intSunX = 20;
+  int intSunY = 20;
+
+  // Variables to manage sun movement
+  boolean isUpPressed = false;
+  boolean isDownPressed = false;
+  boolean isRightPressed = false;
+  boolean isLeftPressed = false;
+  boolean isWPressed = false;
+  boolean isDPressed = false;
+  boolean isSPressed = false;
+  boolean isAPressed = false;
+
+  // Variable for the size of the dog image
+  int imgSize = 50;
+
   /**
-   * Sets the size of the window.
+   * Sets the size of the sketch window.
    */
   public void settings() {
-    size(800, 800);
+    size(1000, 850);
   }
 
   /**
-   * Sets up the initial background color when the sketch starts.
+   * Initializes the sketch with necessary setup.
    */
   public void setup() {
-    background(210, 255, 173);
+    background(255);
+    imgBackground = loadImage("white.png");
+    imgFlower = loadImage("flower.png");
+    imgFlower.resize(50, 0);
+    imgBackground2 = loadImage("green.png");
+    imgDog = loadImage("pepsi.jpg");
+    imgDog.resize(imgSize, imgSize);
   }
 
   /**
-   * Draws continuous elements or animations.
+   * Draws elements on the screen continuously.
    */
   public void draw() {
-    // Sample continuous element (e.g., moving clouds)
-    drawClouds(); // Moving clouds
+    // Display background images
+    image(imgBackground2, 0, height / 2, width, height / 2);
+    image(imgBackground, 0, 0, width, height / 2);
+    image(imgDog, mouseX - imgSize / 2, mouseY - imgSize / 2);
 
-    // Other elements based on user input (mouse and keyboard)
-    // You might not need code for all input events within the draw function,
-    // only those elements that need continuous rendering or updates.
+    // Draw the sun
+    noStroke();
+    fill(255, 223, 34);
+    ellipse(intSunX, intSunY, intSunSizeX, intSunSizeY);
+
+    // Handle adding flowers on click
+    if (isFlowerClicked && mouseY > height / 2) {
+      flowers.add(new PVector(mouseX, mouseY));
+      isFlowerClicked = false;
+    }
+
+    // Display flowers
+    for (PVector pos : flowers) {
+      image(imgFlower, pos.x - imgFlower.width / 8, pos.y - imgFlower.height / 8);
+    }
+
+    // Display raindrops
+    for (int i = raindrops.size() - 1; i >= 0; i--) {
+      Raindrop r = raindrops.get(i);
+      r.update();
+      r.display();
+    }
+
+    // Sets boundaries for where the sun can be
+    if (intSunX > width) {
+      intSunX = width;
+    } else if (intSunX < 0) {
+      intSunX = 0;
+    } else if (intSunY > 220) {
+      intSunY = 220;
+    } else if (intSunY < 0) {
+      intSunY = 0;
+    }
+
+    // Handle multiple key presses for sun movement
+    if (isUpPressed) {
+      intSunSizeY += 2;
+    }
+    if (isDownPressed) {
+      intSunSizeY -= 2;
+    }
+    if (isLeftPressed) {
+      intSunSizeX -= 2;
+    }
+    if (isRightPressed) {
+      intSunSizeX += 2;
+    }
+
+    // Limit the size of the sun
+    if (intSunSizeX > 100) {
+      intSunSizeX = 100;
+    } else if (intSunSizeY > 100) {
+      intSunSizeY = 100;
+    } else if (intSunSizeY < -100) {
+      intSunSizeY = -100;
+    } else if (intSunSizeX < -100) {
+      intSunSizeX = -100;
+    }
+
+    // Use keys to change the location of the sun
+    if (isDPressed) {
+      intSunX += 10;
+    }
+    if (isSPressed) {
+      intSunY += 10;
+    }
+    if (isAPressed) {
+      intSunX -= 10;
+    }
+    if (isWPressed) {
+      intSunY -= 10;
+    }
+
+    // Draw a flower image at the mouse click position if isFlowerClicked is true
+    if (isFlowerClicked) {
+      image(imgFlower, mouseX - imgFlower.width / 8, mouseY - imgFlower.height / 8);
+    }
+
   }
 
   /**
-   * Creates flowers at the mouse position upon mouse click.
-   * @param mouseX The x-coordinate of the mouse.
-   * @param mouseY The y-coordinate of the mouse.
-   */
-  public void mouseClicked() {
-    drawFlower(mouseX, mouseY); // Flower shape
-  }
-
-  /**
-   * Draws grass by drawing lines while dragging the mouse.
-   * @param pmouseX The previous x-coordinate of the mouse.
-   * @param pmouseY The previous y-coordinate of the mouse.
-   * @param mouseX The current x-coordinate of the mouse.
-   * @param mouseY The current y-coordinate of the mouse.
-   */
-  public void mouseDragged() {
-    drawGrass(pmouseX, pmouseY, mouseX, mouseY); // Draw grass while dragging
-  }
-
-  /**
-   * Adds butterflies at the mouse position when the mouse wheel is used.
-   * @param mouseX The x-coordinate of the mouse.
-   * @param mouseY The y-coordinate of the mouse.
-   */
-  public void mouseWheel() {
-    drawButterfly(mouseX, mouseY); // Butterfly shape
-  }
-
-  /**
-   * Changes the background/sky color based on keyboard input.
-   * Pressing 'b' or 'B' changes to a blue sky, 'g' or 'G' changes to green grass.
+   * Handles key press events to set corresponding movement flags.
    */
   public void keyPressed() {
-    if (key == 'b' || key == 'B') {
-      background(0, 0, 255); // Blue sky
-    } else if (key == 'g' || key == 'G') {
-      background(0, 255, 0); // Green grass
+    if (keyPressed) {
+      if (keyCode == UP) {
+        isUpPressed = true;
+      } else if (keyCode == DOWN) {
+        isDownPressed = true;
+      } else if (keyCode == RIGHT) {
+        isRightPressed = true;
+      } else if (keyCode == LEFT) {
+        isLeftPressed = true;
+      } else if (key == 'w' || key == 'W') {
+        isWPressed = true;
+      } else if (key == 'd' || key == 'D') {
+        isDPressed = true;
+      } else if (key == 's' || key == 'S') {
+        isSPressed = true;
+      } else if (key == 'a' || key == 'A') {
+        isAPressed = true;
+      }
     }
   }
 
   /**
-   * Draws clouds with a smoother shape.
+   * Handles key release events to reset corresponding movement flags.
    */
-  private void drawClouds() {
-    noStroke();
-    fill(255); // White clouds
-    ellipse(frameCount % width, 50, 50, 20); // Moving ellipse representing clouds
-  }
-
-  /**
-   * Draws a flower at a given position.
-   * @param x The x-coordinate of the flower.
-   * @param y The y-coordinate of the flower.
-   */
-  private void drawFlower(float x, float y) {
-    int[] colors = { color(200, 51, 73), color(0, 102, 204), color(255, 204, 0) }; // Red, blue, and yellow
-    int randomColorIndex = (int) random(0, colors.length);
-    fill(colors[randomColorIndex]); // Random flower color
-    float petalSize = 20;
-    for (int i = 0; i < 8; i++) {
-        float angle = i * TWO_PI / 8;
-        float petalX = x + cos(angle) * petalSize;
-        float petalY = y + sin(angle) * petalSize;
-        ellipse(petalX, petalY, petalSize, petalSize);
+  public void keyReleased() {
+    if (keyCode == UP) {
+      isUpPressed = false;
+    } else if (keyCode == DOWN) {
+      isDownPressed = false;
+    } else if (keyCode == RIGHT) {
+      isRightPressed = false;
+    } else if (keyCode == LEFT) {
+      isLeftPressed = false;
+    } else if (key == 'w' || key == 'W') {
+      isWPressed = false;
+    } else if (key == 'd' || key == 'D') {
+      isDPressed = false;
+    } else if (key == 's' || key == 'S') {
+      isSPressed = false;
+    } else if (key == 'a' || key == 'A') {
+      isAPressed = false;
     }
-    fill(255, 255, 0); // Center of the flower
-    ellipse(x, y, 10, 10);
-}
-
-  /**
-   * Draws grass at the current and previous mouse positions.
-   * @param prevX The previous x-coordinate of the mouse.
-   * @param prevY The previous y-coordinate of the mouse.
-   * @param currentX The current x-coordinate of the mouse.
-   * @param currentY The current y-coordinate of the mouse.
-   */
-  private void drawGrass(float prevX, float prevY, float currentX, float currentY) {
-    stroke(0, 150, 0); // Green color for grass
-    line(prevX, prevY, currentX, currentY); // Draw grass while dragging
   }
 
   /**
-   * Draws a butterfly at a given position.
-   * @param x The x-coordinate of the butterfly.
-   * @param y The y-coordinate of the butterfly.
+   * Function to handle mouse click events to set the isFlowerClicked flag to
+   * true.
    */
-  private void drawButterfly(float x, float y) {
-    fill(random(100, 220)); // Yellow color for butterflies
-    ellipse(x, y, 15, 10); // Butterfly body
-    fill(random(0,256)); // Blue color for wings
-    float wingSize = 30;
-    float wingOffset = 15;
-    ellipse(x - wingOffset, y, wingSize, (float) (wingSize * 1.2)); // Left wing
-    ellipse(x + wingOffset, y, wingSize, (float) (wingSize * 1.2)); // Right wing
+  public void mousePressed() {
+    isFlowerClicked = true;
+  }
+
+  /**
+   * Function to handle mouse release events to set the isFlowerClicked flag to
+   * false.
+   */
+  public void mouseReleased() {
+    isFlowerClicked = false;
+  }
+
+  /**
+   * Function to handle mouse drag events (rain simulation).
+   */
+  public void mouseDragged() {
+    // Create raindrops for the entire canvas
+    isDragging = true;
+    for (int i = 0; i < 1; i++) { // Create multiple raindrops per frame
+      raindrops.add(new Raindrop(mouseX, mouseY));
+    }
+  }
+
+  /**
+   * Function to adjust the size of the image based on mouse movement.
+   */
+  public void mouseMoved() {
+    imgSize = (int) map(mouseX + mouseY, 0, width + height, 20, 100);
+  }
+
+  /**
+   * Function to handle mouse wheel events.
+   * 
+   * @param event MouseEvent object containing scroll information.
+   */
+  public void mouseWheel(MouseEvent event) {
+    int scrollDirection = event.getCount();
+    int numRaindrops = 1; // Number of raindrops to add/remove
+
+    if (scrollDirection > 0) {
+      for (int i = 0; i < numRaindrops; i++) {
+        raindrops.add(new Raindrop(random(width), random(height)));
+      }
+    } else {
+      for (int i = 0; i < numRaindrops; i++) {
+        if (!raindrops.isEmpty()) {
+          raindrops.remove(0);
+        }
+      }
+    }
+  }
+
+  /**
+   * Raindrop class for raindrop properties and behavior.
+   */
+  class Raindrop {
+    PVector position;
+    float speed;
+
+    /**
+     * Constructor to initialize a raindrop.
+     * 
+     * @param x X-coordinate of raindrop.
+     * @param y Y-coordinate of raindrop.
+     */
+    Raindrop(float x, float y) {
+      position = new PVector(x, y);
+      speed = random(6, 10);
+    }
+
+    /**
+     * Updates raindrop's position.
+     */
+    void update() {
+      position.y += speed;
+      // Remove raindrops that have moved off-screen
+      if (position.y > height) {
+        raindrops.remove(this);
+      }
+    }
+
+    /**
+     * Displays the raindrop on the screen.
+     */
+    void display() {
+      stroke(0, 128, 255); // Raindrop color
+      strokeWeight(2);
+      line(position.x, position.y, position.x, position.y + 10); // Adjust length for raindrop appearance
+    }
   }
 }
